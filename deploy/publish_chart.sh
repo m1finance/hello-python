d02da8a387895a76b9100be060f1c714d54c31c8
Ty Schreiner <t.schreiner@m1finance.com>
2020-05-18 19:45:55 -0500

    environment: filed cannot resolve other vars. Gets set to static string.

--- deploy/publish_chart.sh
+++ deploy/publish_chart.sh
@@ -5,6 +5,7 @@ export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY_ENV_VAR_NAME
 
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 CUR_CHART_VERSION=$(cat chart/Chart.yaml | grep "version:" | awk -F ': ' '{ print $2 }')
-sed -i .bak 's/'$CUR_CHART_VERSION'/'$VERSION'/g' chart/Chart.yaml
+CHART_VERSION="$VERSION+$CIRCLE_SHA1"
+sed -i .bak 's/'$CUR_CHART_VERSION'/'$CHART_VERSION'/g' chart/Chart.yaml
 tar -czvf ./chart.tgz ./chart
 aws s3 cp ./chart.tgz s3://m1-develop-chart-museum/hello-python/${DOCKER_TAG}-chart.tgz
