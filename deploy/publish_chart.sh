d7d21486feacc5754574c0f02916ada764f6f603
Ty Schreiner <t.schreiner@m1finance.com>
2020-05-18 19:42:21 -0500

    attempt to publish the chart and assign symver.

---
+++ deploy/publish_chart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID_ENV_VAR_NAME
+export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY_ENV_VAR_NAME
+
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
+CUR_CHART_VERSION=$(cat chart/Chart.yaml | grep "version:" | awk -F ': ' '{ print $2 }')
+sed -i .bak 's/'$CUR_CHART_VERSION'/'$VERSION'/g' chart/Chart.yaml
+tar -czvf ./chart.tgz ./chart
+aws s3 cp ./chart.tgz s3://m1-develop-chart-museum/hello-python/${DOCKER_TAG}-chart.tgz
