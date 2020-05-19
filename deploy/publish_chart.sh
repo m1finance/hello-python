51396a5c0f66642f4b95908b69c0d6f18ffb3c2b
Ty Schreiner <t.schreiner@m1finance.com>
2020-05-18 20:18:08 -0500

    hoping '.' separated pre-release identifiers accept git hashes.

--- deploy/publish_chart.sh
+++ deploy/publish_chart.sh
@@ -5,7 +5,7 @@ export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY_ENV_VAR_NAME
 
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
 CUR_CHART_VERSION=$(cat chart/Chart.yaml | grep "version:" | awk -F ': ' '{ print $2 }')
-CHART_VERSION="$VERSION-$CIRCLE_SHA1"
+CHART_VERSION="$VERSION.$CIRCLE_SHA1"
 sed -i 's/'$CUR_CHART_VERSION'/'$CHART_VERSION'/g' chart/Chart.yaml
 tar -czvf ./chart.tgz ./chart
 aws s3 cp ./chart.tgz s3://m1-develop-chart-museum/hello-python/${CHART_VERSION}-chart.tgz
