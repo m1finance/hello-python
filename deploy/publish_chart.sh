d1a94ae360af5b8787d9883b267cc8072ebc9a6f
Ty Schreiner <t.schreiner@m1finance.com>
2020-05-18 21:35:18 -0500

    getting sloppy its too late.

--- deploy/publish_chart.sh
+++ deploy/publish_chart.sh
@@ -4,9 +4,9 @@ export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID_ENV_VAR_NAME
 export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY_ENV_VAR_NAME
 
 eval $(aws ecr get-login --region us-east-1 --no-include-email)
-CUR_CHART_VERSION=$(cat chart/Chart.yaml | grep "version:" | awk -F ': ' '{ print $2 }')
-CHART_VERSION="$VERSION.$CIRCLE_SHA1"
-sed -i 's/'$CUR_CHART_VERSION'/'$CHART_VERSION'/g' chart/Chart.yaml
-#tar -czvf ./chart.tgz ./chart
+CUR_CHART_VERSION=$(cat hello-python-chart/Chart.yaml | grep "version:" | awk -F ': ' '{ print $2 }')
+CHART_VERSION="$VERSION-$CIRCLE_SHA1"
+sed -i 's/'$CUR_CHART_VERSION'/'$CHART_VERSION'/g' hello-python-chart/Chart.yaml
+#tar -czvf ./chart.tgz ./hello-python-chart
 helm package ./hello-python-chart
-aws s3 cp ./chart.tgz s3://m1-develop-chart-museum/hello-python/*.tgz
+aws s3 cp ./*.tgz s3://m1-develop-chart-museum/hello-python/
