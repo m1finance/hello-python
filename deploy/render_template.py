dc9d7e509318f176de38633cd3e3c78c2e7a2ed0
Ty Schreiner <t.schreiner@m1finance.com>
2020-05-26 13:40:40 -0500

    add tag to commit, don't print data into committed file.

--- deploy/render_template.py
+++ deploy/render_template.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
  build_uid = '-'.join([os.environ['VERSION'], os.environ['CIRCLE_BUILD_NUM'], os.environ['CIRCLE_SHA1']])
  config_data["image"]["tag"]=build_uid
  config_data["chart"]["version"]=build_uid
- print(config_data)
  env = Environment(loader = FileSystemLoader('./templates'),   trim_blocks=True, lstrip_blocks=True)
  template = env.get_template('hello-python-hr.yml.j2')
  print(template.render(config_data))
